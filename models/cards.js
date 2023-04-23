693b0da26647a0ab413c1658e28b33f60a2fa840
h1ze <burgov@mail.ru>
2023-04-23 15:44:58 +0400

    feat: создать схему Card

---
+++ models/cards.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: new mongoose.Types.ObjectId(),
+    ref: 'user',
+    required: true,
+  },
+  likes: {
+    type: new mongoose.Types.ObjectId(),
+    ref: 'user',
+    required: true,
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('card', cardSchema);
