ecb93473d071b676651d8afb8da2db02d4ddf676
h1ze <burgov@mail.ru>
2023-04-25 22:00:20 +0400

    fix: app.use(express.json())

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const app = express();
 const { PORT = 3000 } = process.env;
 
+app.use(express.json());
+
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   // useCreateIndex: true,
