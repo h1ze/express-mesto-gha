772fd93ab0aa57e19e53cc0b5b8287bbb175db6e
h1ze <burgov@mail.ru>
2023-04-29 16:51:17 +0400

    fix: переписать обработку ошибки 400

--- controllers/users.js
+++ controllers/users.js
@@ -22,16 +22,19 @@ module.exports.createUser = (req, res) => {
 module.exports.getUserByID = (req, res) => {
   User.findById(req.params.userId)
     .orFail(() => {
-      throw new Error('Не найден пользователь');
+      throw new Error('Запрашиваемый пользователь не найден');
     })
     .then((user) => {
       res.send({ data: user });
     })
     .catch((err) => {
-      if (err.message === 'Не найден пользователь') {
+      if (err.message === 'Запрашиваемый пользователь не найден') {
         res.status(404).send({ message: err.message });
-      } else if (err.message.includes('failed')) {
-        res.status(400).send({ message: err.message });
+      } else if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map((error) => error.message).join('; ');
+        res.status(400).send({ message });
+      // } else if (err.message.includes('failed')) {
+      //   res.status(400).send({ message: err.message });
       } else {
         res.status(500).send({ message: err.message });
       }
