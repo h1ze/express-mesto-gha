2877c90a5a4f57122e38e53b6492ffa6ec72f18a
h1ze <burgov@mail.ru>
2023-05-08 19:11:14 +0400

    fix: тест RegExp

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.post('/signup', celebrate({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
     // eslint-disable-next-line prefer-regex-literals
-    avatar: Joi.string().required().pattern(new RegExp('^(http|https|ftp)://(([A-Z0-9][A-Z0-9_-]*)(.[A-Z0-9][A-Z0-9_-]*)+)')),
+    avatar: Joi.string().required().pattern(new RegExp('^(http|https|ftp)://(w+.w+.w+)/*w*.*w*')),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
