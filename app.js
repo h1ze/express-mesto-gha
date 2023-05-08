82437edbbf4d981ca6cfa0a48aabd9fb27eeb55a
h1ze <burgov@mail.ru>
2023-05-08 11:23:56 +0400

    feat: вынести коды ошибок в константу

--- app.js
+++ app.js
@@ -44,6 +44,8 @@ app.use((err, req, res, next) => {
       ? 'На сервере произошла ошибка'
       : message,
   });
+
+  next();
 });
 
 app.listen(PORT, () => {
--- errors/bad-request-err.js
+++ errors/bad-request-err.js
@@ -1,8 +1,8 @@
-class CastError extends Error {
+class BadRequestError extends Error {
   constructor(message) {
     super(message);
     this.statusCode = 400;
   }
 }
 
-module.exports = CastError;
+module.exports = BadRequestError;
