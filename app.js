3709fb7ef35ef8b6cfa57feedc94f10f6868afa1
h1ze <burgov@mail.ru>
2023-05-10 01:22:12 +0400

    feat: добавить Joi валидацию для /signin

--- app.js
+++ app.js
@@ -23,7 +23,13 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 app.use(helmet());
 
 // роуты, не требующие авторизации,
-app.post('/signin', login);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
