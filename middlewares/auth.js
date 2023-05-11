2ca94db38a33b807500c289f4be6c5e63e68e3aa
h1ze <burgov@mail.ru>
2023-05-11 12:52:29 +0400

    fix: исправить auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,21 +3,20 @@ const AuthError = require('../errors/auth-err');
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  // достаём авторизационный заголовок
-  const { authorization } = req.headers;
-
-  // убеждаемся, что он есть или начинается с Bearer
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new AuthError('Необходима авторизация');
-  }
-
-  // Убираем Bearer, оставляем только строку с токеном
-  const token = authorization.replace('Bearer ', '');
-
-  // верифицируем токен
   let payload;
   try {
-    // попытаемся верифицировать токен
+  // достаём авторизационный заголовок
+    const { authorization } = req.headers;
+
+    // убеждаемся, что он есть или начинается с Bearer
+    if (!authorization || !authorization.startsWith('Bearer ')) {
+      throw new AuthError('Необходима авторизация');
+    }
+
+    // Убираем Bearer, оставляем только строку с токеном
+    const token = authorization.replace('Bearer ', '');
+
+    // верифицируем токен
     payload = checkToken(token);
   } catch (err) {
     // отправим ошибку, если не получилось
